ef396d2cc9a9d67d5a62b13f23500fd5b98d5451
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-07-17 10:54:51 +0000

    Added adding songs

--- main.py
+++ main.py
@@ -3,10 +3,36 @@ from signal import signal, SIGINT
 Singnal and SIGINIT are used to handle CTRL-C and SIGINT, and exit gracefully.
 """
 
-import os, external_modules, platform, pandas
+import os, external_modules, pandas
 
 def handler(signal_received, frame):
     # Handling any cleanup here
     print('\nCTRL-C or SIGINT detected. Exiting gracefully...')
     exit(0)
 
+
+### Main menu ###
+while True:
+    signal(SIGINT, handler)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("\033[1\033[92mWelcome to the Music Quiz:\033[00m\033[0m")
+    choice = input("""
+Enter '\033[1madd\033[0m' to add custom songs to the quiz
+Enter '\033[1mplay\033[0m' to play the quiz
+Enter '\033[1mquit\033[0m' to exit the program
+""")
+    if choice == "add":
+        # Add songs to the quiz
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("\033[1\033[92mWelcome to the Music Quiz:\033[00m\033[0m")
+        print("\033[1mAdd songs to the quiz:\033[00m")
+        print("\033[1mEnter '\033[0mback\033[1m' to return to the main menu\033[0m")
+        while True:
+            song_name = input("Enter the name of the song: ")
+            if song_name == "back":
+                break
+            artist_name = input("Enter the name of the artist: ")
+            if artist_name == "back":
+                break
+            external_modules.writing_data_csv("quiz_songs.csv", [song_name, artist_name], ["song_name", "artist_name"])
+            print("\033[1mSong added successfully!\033[00m")
